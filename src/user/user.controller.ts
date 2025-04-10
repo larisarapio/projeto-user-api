1b6045e4037ca7a6279c307a5809460c740801db
Larissa <larissa.sarapio@gmail.com>
2025-04-10 00:05:41 -0300

    edit: crud inicial

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,41 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller()
 export class UserController {
   @Get()
-  async getUser(): Promise<any> {
+  getUser() {
     const user = {};
     return user;
   }
 
   @Post()
-  async createUser(@Body() body: any): Promise<any> {
+  createUser(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body) {
+    return {
+      message: `Usuário ${id} atualizado!`,
+      data: body,
+    };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string, @Body() body) {
+    return {
+      message: `Usuário ${id} deletado!`,
+      data: body,
+    };
   }
 }
