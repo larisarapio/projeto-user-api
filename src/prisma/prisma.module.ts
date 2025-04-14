c7b3e0fc2ec7cd3d4d45cf60e68b9257b333baf1
larisarapio <larissa.sarapio@gmail.com>
2025-04-14 18:54:51 -0300

    feat: implementação prisma

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
+
+@Module({
+// aqui está dizendo q ele faz para dessse modulo prima module.
+  providers: [PrismaService],
+  //qm usar esse módulo vai ter acesso a isso
+  exports: [PrismaService]
+})
+export class PrismaModul {}
