90974aa3ced22b57da5860c8d59cbd6b2c57ccd8
larisarapio <larissa.sarapio@gmail.com>
2025-04-23 23:41:51 -0300

    edit: ajuste error name

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,3 +1,8 @@
+import { IsString } from "class-validator";
 import { AuthLoginDTO } from "./auth-login.dto";
 
-export class AuthRegisterDTO extends AuthLoginDTO {}
+export class AuthRegisterDTO extends AuthLoginDTO {
+    
+    @IsString()
+    name: string;
+}
